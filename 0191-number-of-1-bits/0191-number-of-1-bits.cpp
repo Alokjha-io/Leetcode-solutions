3c39646653bc348d5683435738e2d11f73991098
Alok Kumar jha <alokkumarjhahome@gmail.com>
2026-08-19 18:36:20 +0530

    Time: 0 ms (100%), Space: 8.3 MB (11.07%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int hammingWeight(int n) {
+        unsigned int num = n;
+        int count = 0;
+        while(num)
+        {
+            num &= (num-1);
+            count++;
+        }
+        return count;
+    }
+};
