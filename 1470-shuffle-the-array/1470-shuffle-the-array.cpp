04ea2210f8b4cdd07329d0df99972fb32b5f0575
Alok Kumar jha <alokkumarjhahome@gmail.com>
2026-09-06 19:41:16 +0530

    Time: 4 ms (65.43%), Space: 13.5 MB (52.73%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.cpp
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
         vector<int> result;
-        int i = 0;
-        int j = n;
+
+
         for(int x=0;x<n;x++)
         {
-            result.push_back(nums[x+i]);
-            result.push_back(nums[x+j]);
+            result.push_back(nums[x]);
+            result.push_back(nums[x+n]);
         }
         return result;
     }
